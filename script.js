9abb3e3193b703d3a858b09adafb4fa1c8ec1b01
Ahmet <ahmtyln.06@gmail.com>
2021-08-11 22:47:02 +0300

    commit-1

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ var models = [
     {
         name : "Bmw 418d",
         image : "img/bmw.jpg",
-        link :"http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe"
+        link :"https://www.google.com.tr/"
     },
     {
         name : "Mazda CX-3",
         image : "img/mazda.jpg",
-        link :"http://www.arabalar.com.tr/mazda/cx-3/2017/1-5-sky-d-motion"
+        link :"https://www.google.com.tr/"
     },
     {
         name : "Volvo S60",
         image : "img/volvo.jpg",
-        link :"http://www.arabalar.com.tr/volvo/s60/2018/1-5-t3-advance"
+        link :"https://www.google.com.tr/"
     },
     {
         name : "Skoda Superb",
         image : "img/skoda.jpg",
-        link :"http://www.arabalar.com.tr/skoda/superb/2018/1-4-tsi-active"
+        link :"https://www.google.com.tr/"
     },
     {
         name : "honda Civic",
         image : "img/honda.jpg",
-        link :"http://www.arabalar.com.tr/honda/civic/2018/1-6-elegance"
+        link :"https://www.google.com.tr/"
     }
 ]
 
